d441acfe62496b3730978d5bbaf05ac0ce7375c4
Abdev23 <agadir.9ine98@gmail.com>
2023-12-11 02:52:10 +0100

    test files

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Test suite for Amenity class
+"""
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = Amenity()
+
+    def tearDown(self):
+        del self.new_user
+
+    def test_user_type(self):
+        self.assertEqual(self.new_user.name, "")
+
+    def test_user_attribute(self):
+        self.new_user.name = "Root"
+        self.assertEqual(self.new_user.name, "Root")
+
+
+if __name__ == '__main__':
+    unittest.main()
