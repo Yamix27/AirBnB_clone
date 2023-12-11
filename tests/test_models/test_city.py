c353d0065ce5a7b92c3879b19268ecc0659d56ba
Abdev23 <agadir.9ine98@gmail.com>
2023-12-11 03:01:23 +0100

    adding the test files

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Test suite for City class
+"""
+import unittest
+from models.city import City
+from models.state import State
+
+
+class TestAmenity(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = City()
+        self.s_id = State()
+
+    def tearDown(self):
+        del self.new_user
+
+    def test_user_type(self):
+        self.assertEqual(self.new_user.state_id, "")
+        self.assertEqual(self.new_user.name, "")
+
+    def test_user_attribute(self):
+        self.new_user.name = "Root"
+        self.new_user.state_id = self.s_id.id
+
+        self.assertEqual(self.new_user.name, "Root")
+        self.assertEqual(self.new_user.state_id, self.s_id.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
